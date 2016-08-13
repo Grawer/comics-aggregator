29f28ad87c09ea3e2d68cf3992d6d20b9465f7d4
Damian Świerad <damian.swierad@gmail.com>
2016-08-14 00:52:46 +0200

    Fixed adding of empty comic url.

--- cron.php
+++ cron.php
@@ -5,21 +5,25 @@ require_once './vendor/autoload.php';
 $comic = new \Grawer\ComicsAggregator\Source\Garfield();
 $imageUrl = $comic->getLatestComicImageUrl();
 
-$comicItem = new \Grawer\ComicsAggregator\Entity\ComicItem();
-$comicItem->title = 'Garfield';
-$comicItem->url = $imageUrl;
-$comicItem->description = '';
+if (!empty($imageUrl)) {
+    $comicItem = new \Grawer\ComicsAggregator\Entity\ComicItem();
+    $comicItem->title = 'Garfield';
+    $comicItem->url = $imageUrl;
+    $comicItem->description = '';
 
-$repository = new \Grawer\ComicsAggregator\Entity\ComicItemRepository();
-$repository->save($comicItem);
+    $repository = new \Grawer\ComicsAggregator\Entity\ComicItemRepository();
+    $repository->save($comicItem);
+}
 
 $comic = new \Grawer\ComicsAggregator\Source\Dillbert();
 $imageUrl = $comic->getLatestComicImageUrl();
 
-$comicItem = new \Grawer\ComicsAggregator\Entity\ComicItem();
-$comicItem->title = 'Dillbert';
-$comicItem->url = $imageUrl;
-$comicItem->description = '';
+if (!empty($imageUrl)) {
+    $comicItem = new \Grawer\ComicsAggregator\Entity\ComicItem();
+    $comicItem->title = 'Dillbert';
+    $comicItem->url = $imageUrl;
+    $comicItem->description = '';
 
-$repository = new \Grawer\ComicsAggregator\Entity\ComicItemRepository();
-$repository->save($comicItem);
+    $repository = new \Grawer\ComicsAggregator\Entity\ComicItemRepository();
+    $repository->save($comicItem);
+}
