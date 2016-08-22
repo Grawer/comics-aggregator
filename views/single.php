ef80aa773a66a05e9f747c8c8ee083a0fe5806c3
Damian Świerad <damian.swierad@gmail.com>
2016-08-22 21:05:24 +0200

    Addded some Lorem Ipsum to fool GetPocket article parser.

--- views/single.php
+++ views/single.php
@@ -10,14 +10,40 @@
             <h2><?php echo $comic->title; ?></h2>
             <?php } ?>
             <img src="<?php echo $comic->url; ?>" alt=""/>
-            <p>
             <?php
                 if (!empty($comic->description)) {
+            ?>
+            <p>
+            <?php
                     echo $comic->description;
-                } else {
-                    echo 'Comic id ' . $comic->id;
+            ?>
+            </p>
+            <?php
                 }
             ?>
+            <p>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+            </p>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.            
             </p>
         </article>
     </body>
