c3a30367d0f013f4e5502e3c85b0aab8290fd7b0
Damian Świerad <damian.swierad@gmail.com>
2016-11-14 18:21:35 +0100

    Fixed AndrzejRysuje source.

--- src/ComicsAggregator/Source/AndrzejRysuje.php
+++ src/ComicsAggregator/Source/AndrzejRysuje.php
@@ -5,24 +5,20 @@ namespace Grawer\ComicsAggregator\Source;
 class AndrzejRysuje extends Base
 {
     protected $homepage;
+    protected $json;
 
     public function getLatestComicImageUrl()
     {
-        $this->homepage = file_get_contents('http://www.andrzejrysuje.pl/');
+        $this->homepage = file_get_contents('http://www.andrzejrysuje.pl/wordpress/wp-json/wp/v2/posts?page=1');
+        $this->homepage = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $this->homepage);
+        $this->json = json_decode($this->homepage, true);
 
-        preg_match(
-            '!<div class="post-.*?<img class=".*?wp-image-.*?" src="(.*?)"!sm',
-            $this->homepage,
-            $matches
-        );
-
-        if (isset($matches[1])) {
-            $url = $matches[1];
-
-            return $url;
+        $url = false;
+        if (isset($this->json[0]['post_attachment'])) {
+            $url = $this->json[0]['post_attachment'];
         }
 
-        return false;
+        return $url;
     }
 
     public function getTitle()
@@ -31,39 +27,11 @@ class AndrzejRysuje extends Base
             $this->getLatestComicImageUrl();
         }
 
-        preg_match(
-            '!<div class="post-.*?<h2.*?>(.*?)</h2>!sm',
-            $this->homepage,
-            $matches
-        );
-
-        if (isset($matches[1])) {
-            $clean = strip_tags($matches[1]);
-
-            return $clean;
+        $title = '';
+        if (isset($this->json[0]['title']['rendered'])) {
+            $title = $this->json[0]['title']['rendered'];
         }
 
-        return '';
-    }
-
-    public function getDescription()
-    {
-        if (empty($this->homepage)) {
-            $this->getLatestComicImageUrl();
-        }
-
-        preg_match(
-            '!<div class="post-.*?<img.*?/>(.*?)</p>!sm',
-            $this->homepage,
-            $matches
-        );
-
-        if (isset($matches[1])) {
-            $clean = strip_tags($matches[1]);
-
-            return $clean;
-        }
-
-        return '';
+        return $title;
     }
 }
