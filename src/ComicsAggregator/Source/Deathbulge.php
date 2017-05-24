2c05823a19aeb6a28a9813521385fb2013313555
Damian Świerad <damian.swierad@gmail.com>
2017-05-24 22:00:00 +0200

    Added Deathbulge source. Turned off PHD Comics.

---
+++ src/ComicsAggregator/Source/Deathbulge.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Grawer\ComicsAggregator\Source;
+
+class Deathbulge extends Base
+{
+    public function getLatestComicImageUrl()
+    {
+        $entry = file_get_contents('http://www.deathbulge.com/api/comics');
+        $this->entry = json_decode($entry, true);
+
+        if (isset($this->entry['comic']['comic'])) {
+            $url = 'http://www.deathbulge.com' . $this->entry['comic']['comic'];
+
+            return $url;
+        }
+
+        return false;
+    }
+
+    public function getTitle()
+    {
+        if (isset($this->entry['comic']['title'])) {
+            return $this->entry['comic']['title'];
+        }
+
+        return '';
+    }
+
+    public function getDescription()
+    {
+        if (isset($this->entry['comic']['alt_text'])) {
+            return $this->entry['comic']['alt_text'];
+        }
+
+        return '';
+    }
+}
